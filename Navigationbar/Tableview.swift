1b770fc9f2e5fd1042d640e75dd6d9712030ec43
Ali <alirezamonfared813@gmail.com>
2023-05-17 09:00:29 -0700

    ss2

---
+++ Navigationbar/Tableview.swift
@@ -0,0 +1,19 @@
+//
+//  Tableview.swift
+//  Navigationbar
+//
+//  Created by Alireza Karimi on 2023-05-17.
+//
+
+import UIKit
+
+class SnacksTableViewCell: UITableViewCell {
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
